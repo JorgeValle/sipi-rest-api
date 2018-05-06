c59f52e3bb767ae09c617792f5f5435eac298480
jvalle <jvalle@veic.org>
2018-05-05 22:21:17 -0400

    Added a few more methods to environment service.

--- services/environment.service.js
+++ services/environment.service.js
@@ -12,3 +12,31 @@ module.exports.returnMailgun = function() {
   }
 
 }
+
+/**
+ * Returns the password for the web API
+ * @returns {string} - the password for the various endpoints
+ */
+module.exports.returnApiPassword = function() {
+
+  return process.env.API_PASSWORD;
+
+}
+
+/**
+ * Returns the URI for the Mongo database
+ */
+module.exports.returnDbUri = function() {
+
+  return process.env.MONGOLAB_URI;
+
+}
+
+/**
+ * Returns the secret for the JSON web token
+ */
+module.exports.returnJwtSecret = function() {
+
+  return process.env.JWT_SECRET;
+
+}
