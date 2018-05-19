6c7820b181d07a21b947a78a03618dea8d371e58
jvalle <jvalle@veic.org>
2018-05-19 15:48:41 -0400

    First pass at a diacritic accent replacer, meant for url.

--- services/url.service.js
+++ services/url.service.js
@@ -1,14 +1,42 @@
 'use strict';
 
 /**
- * Sluggifies the string: trimming, replacing spaces and lowercasing
- * @param {string} stringToSlug - the user object
+ * Replaces accented characters with non-accented
  */
-module.exports.sluggify = function(stringToSlug) {
+const replaceDiacritics = function(str) {
+
+  const replacementMap = new Map();
+
+  replacementMap.set('a', 'á');
+  replacementMap.set('e', 'é');
+  replacementMap.set('i', 'í');
+  replacementMap.set('o', 'ó');
+  replacementMap.set('u', 'ú');
+  replacementMap.set('n', 'ñ');
+  replacementMap.set('u', 'ü');
+
+  // swap out all the instances of accented characters
+  replacementMap.forEach(function(item, key) {
+    str = str.replace(item, key);
+  });
+
+  return str;
+
+}
+
+/**
+ * Sluggifies the string: trimming, replacing spaces and lowercasing
+ * @param {string} str - the string to sluggify
+ */
+module.exports.sluggify = function(str) {
   
   try {
 
-    let sluggified = stringToSlug.trim().split(' ').join('-').toLowerCase();
+    // prettify...
+    let sluggified = str.trim().split(' ').join('-').toLowerCase();
+
+    // and also replace diacritics
+    sluggified = replaceDiacritics(sluggified);
 
     return sluggified;
 
